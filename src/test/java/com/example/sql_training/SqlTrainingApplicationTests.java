9fd4e66cd9dcee9e63cc5d5c94a58151d41a87b4
elenazorkina <elena.zorkina@cengage.com>
2024-08-14 17:08:06 +0300

    first commit

---
+++ src/test/java/com/example/sql_training/SqlTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
