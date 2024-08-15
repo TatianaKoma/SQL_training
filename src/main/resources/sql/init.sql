6553c1f64decf830900b11701c7c65cbbeea6d61
elenazorkina <elena.zorkina@cengage.com>
2024-08-15 10:47:05 +0300

    task_create_table: column group_id was added

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,14 +1,15 @@
-CREATE TABLE groups
+CREATE TABLE IF NOT EXISTS groups
 (
-    id   BIGINT PRIMARY KEY NOT NULL,
+    id   BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE students
+CREATE TABLE IF NOT EXISTS students
 (
-    id            BIGINT PRIMARY KEY NOT NULL,
+    id            BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name          TEXT      NOT NULL,
     surname       TEXT      NOT NULL,
     date_of_birth TIMESTAMP NOT NULL,
+    group_id      BIGINT NOT NULL,
     CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES groups (id)
 );
